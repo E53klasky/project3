131fecad94e7b56fbbf6046aed343284bff2c5dd
E53klasky <klaskyethan@gmail.com>
2024-10-21 21:16:04 -0400

    Update image.cpp

--- image.cpp
+++ image.cpp
@@ -1,11 +1,32 @@
 #include <iostream> 
 #include <vector>
 #include <string>
-
-using namespace std;
+#include <fstream>
+using namespace std; 
 
 int main()
 {
+    // testing this RIGHT NOW NOT ACTAUL CODE
+      std::ofstream outFile("example.txt");
+    if (outFile.is_open()) {
+        outFile << "Hello, this is a test file.\n";
+        outFile << "Writing multiple lines of text here...\n";
+        outFile.close();
+    } else {
+        std::cerr << "Unable to open file for writing.\n";
+    }
+
+    
+    std::ifstream inFile("example.txt");
+    std::string line;
+    if (inFile.is_open()) {
+        while (std::getline(inFile, line)) {
+            std::cout << line << '\n';
+        }
+        inFile.close();
+    } else {
+        std::cerr << "Unable to open file for reading.\n";
+    }
     cout<<"hello world \n";
     return 0;
 }
