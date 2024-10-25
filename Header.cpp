1ebf990bd1282738ee40db0d1acf84c4a92fb1d1
Ethan Klasky <klaskyethan@gmail.com>
2024-10-24 22:37:53 -0400

    changed

--- Header.cpp
+++ Header.cpp
@@ -1,27 +1,21 @@
-#include <string>
 #include <iostream>
+#include "structHeader.h"
 using namespace std; 
 
-struct Header
+void printHead(const Header& head)
 {
-char idLength;
-char colorMapType;
-char dataTypeCode;
-short colorMapOrigin;
-short colorMapLength;
-char colorMapDepth;
-short xOrigin;
-short yOrigin;
-short width;
-short height;
-char bitsPerPixel;
-char imageDescriptor;
-};
-
-
-void head()
-{
-cout<<"hello";
+cout<<"The ID Length is: "<<head.idLength<<endl;
+cout<<"The Color Map Type is: "<<head.colorMapDepth<<endl;
+cout<<"The Data Type Code is: "<<head.dataTypeCode<<endl;
+cout<<"The Color Map Origin is: "<<head.colorMapOrigin<<endl;
+cout<<"The Color Map Length is: "<<head.colorMapLength<<endl;
+cout<<"The Color Map Depth is: "<<head.colorMapDepth<<endl;
+cout<<"The X-Origin is: "<<head.xOrigin<<endl;
+cout<<"The Y-Origin is: "<<head.yOrigin<<endl;
+cout<<"The Width is: "<<(short)head.width<<endl;
+cout<<"The Height is: "<<(short)head.height<<endl;
+cout<<"The Bits Per Pixel is: "<<head.bitsPerPixel<<endl;
+cout<<"The Image Descriptor is: "<<head.imageDescriptor<<endl;
     
 }
 
