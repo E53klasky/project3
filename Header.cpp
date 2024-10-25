57e5802ca91e7ab8757cce74485ca221cfd90af3
Ethan Klasky <klaskyethan@gmail.com>
2024-10-25 16:54:11 -0400

    I casted the vars for the print to some them

--- Header.cpp
+++ Header.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 void printHead(const Header& head)
 {
-cout<<"The ID Length is: "<<head.idLength<<endl;
-cout<<"The Color Map Type is: "<<head.colorMapDepth<<endl;
-cout<<"The Data Type Code is: "<<head.dataTypeCode<<endl;
-cout<<"The Color Map Origin is: "<<head.colorMapOrigin<<endl;
-cout<<"The Color Map Length is: "<<head.colorMapLength<<endl;
-cout<<"The Color Map Depth is: "<<head.colorMapDepth<<endl;
-cout<<"The X-Origin is: "<<head.xOrigin<<endl;
-cout<<"The Y-Origin is: "<<head.yOrigin<<endl;
+cout<<"The ID Length is: "<<(char)head.idLength<<endl;
+cout<<"The Color Map Type is: "<<(char)head.colorMapDepth<<endl;
+cout<<"The Data Type Code is: "<<(char)head.dataTypeCode<<endl;
+cout<<"The Color Map Origin is: "<<(short)head.colorMapOrigin<<endl;
+cout<<"The Color Map Length is: "<<(short)head.colorMapLength<<endl;
+cout<<"The Color Map Depth is: "<<(char)head.colorMapDepth<<endl;
+cout<<"The X-Origin is: "<<(short)head.xOrigin<<endl;
+cout<<"The Y-Origin is: "<<(short)head.yOrigin<<endl;
 cout<<"The Width is: "<<(short)head.width<<endl;
 cout<<"The Height is: "<<(short)head.height<<endl;
-cout<<"The Bits Per Pixel is: "<<head.bitsPerPixel<<endl;
-cout<<"The Image Descriptor is: "<<head.imageDescriptor<<endl;
+cout<<"The Bits Per Pixel is: "<<(char)head.bitsPerPixel<<endl;
+cout<<"The Image Descriptor is: "<<(char)head.imageDescriptor<<endl;
     
 }
 
