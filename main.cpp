fdce940b7e7e0f56e70f324eb8c1fb9934084b0f
Ethan Klasky <klaskyethan@gmail.com>
2024-11-03 21:09:46 -0500

    here is what I have done here

--- main.cpp
+++ main.cpp
@@ -6,15 +6,53 @@
 using namespace std;
 
 int main()
-{
-    string fileName = "../input/circles.tga";
+{//
+    string fileName = "../input/car.tga";
     image* myImage = new image;
 
     myImage->TGAReader(fileName);
     image::Header header = myImage->getHeader();
     myImage->printHeader(header);
 
-    delete myImage;
-    myImage = nullptr;
+
+
+    int k = 3000;
+    // It works for i = 267928 for red
+        //Segmentation fault(core dumped)
+    // It works for i = 268966 for green
+    // Segmentation fault (core dumped)
+
+    // It works for i = B 267927 It works for i = B 267928 for blue doesn't work with that times 3 ??????
+    // Segmentation fault (core dumped)
+
+
+
+    myImage->scaleImageColor(k , "blue");
+
+
+
+
+
+
+
+    // no core dump 
+    //myImage->rotate180();
+
+  //  myImage->scaleImageColor(k , "red");
+    // It works for i = 268966
+    // Segmentation fault (core dumped)
+
+
+
+    // DO THIS AT THE END FOR EVERYTHING !!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+
+
+
+
+
     return 0;
 }
