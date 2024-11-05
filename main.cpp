2e43dd1c01cbb0e8fd685b07b4ac2bebfce31874
Ethan Klasky <klaskyethan@gmail.com>
2024-11-04 22:10:32 -0500

    I added the task as comments

--- main.cpp
+++ main.cpp
@@ -5,54 +5,86 @@
 #include <fstream>
 using namespace std;
 
+
+// void taskOne()
+// {
+
+// }
+
+
+// void taskTwo()
+// {
+
+// }
+
+
+
+// void taskThree()
+// {
+
+// }
+
+
+// void taskFour()
+// {
+
+// }
+
+
+// void taskFive()
+// {
+
+// }
+
+
+// void taskSix()
+// {
+
+// }
+
+
+// void taskSeven()
+// {
+
+// }
+
+// void taskEight()
+// {
+
+// }
+
+
+// void tasknine();
+// {
+
+// }
+
+// void taskTen();
+// {
+
+// }
+
+
+// void bonus()
+// {
+
+// }
+
+
+
+
 int main()
-{//
-    string fileName = "../input/car.tga";
-    image* myImage = new image;
+{
+  string fileName = "../input/car.tga";
+  image* myImage = new image;
 
-    myImage->TGAReader(fileName);
-    image::Header header = myImage->getHeader();
-    myImage->printHeader(header);
+  myImage->TGAReader(fileName);
+  image::Header header = myImage->getHeader();
+  myImage->printHeader(header);
 
-
-
-    int k = 3000;
-    // It works for i = 267928 for red
-        //Segmentation fault(core dumped)
-    // It works for i = 268966 for green
-    // Segmentation fault (core dumped)
-
-    // It works for i = B 267927 It works for i = B 267928 for blue doesn't work with that times 3 ??????
-    // Segmentation fault (core dumped)
+  delete myImage;
+  myImage = nullptr;
 
 
-
-    myImage->scaleImageColor(k , "blue");
-
-
-
-
-
-
-
-    // no core dump 
-    //myImage->rotate180();
-
-  //  myImage->scaleImageColor(k , "red");
-    // It works for i = 268966
-    // Segmentation fault (core dumped)
-
-
-
-    // DO THIS AT THE END FOR EVERYTHING !!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
-
-
-
-
-
-    return 0;
+  return 0;
 }
